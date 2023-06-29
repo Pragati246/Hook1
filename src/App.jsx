ac3ab9d41c8f6c21b464eefc74efbf3b8d36182f
Pragati246 <pragatitomar246@gmail.com>
2023-06-30 01:00:05 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,24 @@
-import React, {useState} from 'react'
-
-// function initialValue(){
-//   connsole.log("function called!")
-//   return 0;
-// }
+import React, {useState, useEffect} from 'react'
 
 function App() {
-  /*create a var */
-  // let a = "Dipesh";
-  const [name, setName] = useState("Dipesh");
-  // use in boolean
-  const [flag, setFlag] = useState("false");
-  // use in numbers
-  // const [steps, setSteps] = useState(initialValue())
-  const [steps, setSteps] = useState(() =>{
-    console.log("clicked")
-  return 0;
-  });
-  // use in name 
-  const [names, setNames] = useState([]);
-  function changeName(){
-    console.log("clicked");
-    // return setName("Nitesh");
-    return setFlag(!flag);
+
+  const [count, setCount] = useState(0);
+  const [data , setData] = useState("Ram"); 
+  useEffect( ()=>{
+    console.log("component mounted")
+  }, [data, count])
+
+  function changeCount(){
+    return setCount(count + 1);
 }
-function increment() {
-  console.log("clicked")
-  setSteps((prevState) => prevState + 2)
-  // setSteps((prevState) => prevState + 1)
-  // return setSteps(steps + 1);
-}
-function decrement() {
-  console.log("clicked")
-  return setSteps(steps - 1);
-}
-function addNames(e){
-  e.preventDefault();
-  setNames([...names, {id:names.length,name}]);
-  setName("") 
-}
+  function changeData(){
+    return setData("seeta");
+  }
 return (
     <div className="App">
-      <h1>Hello , {flag ? name : " "} </h1>
-      <button onClick={changeName}>click Me</button>
-      <hr></hr>
-      <button onClick={increment}>+</button>
-      <div>{steps}</div>
-      <button onClick={decrement}>-</button>
-      <hr></hr>
-      <form onSubmit={addNames}>
-        <input type="text"
-        value={name}
-        placeholder="add names"
-        onChange={ (e) => setName(e.target.value)}></input>
-        <button>Submit</button>
-      </form>
-      {/* for display names which we added after clicking on submit button */}
-      <ul>
-        {names.map((items) =>(
-          <li key = {items.id}>{items.name}</li>
-        ))}
-      </ul>
+      <h1>Button clicked {count} times {data} </h1>
+      <button onClick={changeCount}>click Me</button>
+      <button onClick={changeData}>updateData</button>
       
     </div>
   )
