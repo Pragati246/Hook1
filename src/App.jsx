545cb4369336cf2fa9602626a5bc58aa64bf17e7
Pragati246 <pragatitomar246@gmail.com>
2023-06-30 10:19:25 +0530

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import FComponenet from './Component/FComponenet';
+import React, {useState, useRef} from 'react'
 
 function App() {
-  const [flag, setFlag] = useState(true);
-return (
-    <div className="App">
-      <div>
-        <button onClick= {() => setFlag(!flag)}>Toggle Function Component</button>
-      </div>
-      {flag ? <FComponenet/> : "" }
-      
+  const refElement = useRef("");
+  const [name, setName] = useState("Pragya");
+  console.log(refElement);
+  function Reset(){
+    console.log("reset")
+    setName(" ")
+    // manipulate DOM directly using ref element
+    // we have our current element under in ref Element
+    refElement.current.focus();
+  }
+  function handleInput(){
+    refElement.current.style.color="blue"
+    refElement.current.style.backgroundColor="pink"
+    refElement.current.value="jerry"
+  }
+  return (
+    <div>
+      <h1>Hellow</h1>
+      <input ref={refElement} type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={handleInput}>handle inputs</button>
     </div>
   )
 }
-export default App;
+
+export default  App;
